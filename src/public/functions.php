f83ab84ee422e896ff5f642ade6d75a670101ee8
adamsow <adam@asowinski.com>
2017-07-14 22:26:25 +0200

    API functions

--- src/public/functions.php
+++ src/public/functions.php
@@ -1,13 +1,59 @@
 <?php
 use \Firebase\JWT\JWT;
 
-function hello($db, $log, $dbw)
+function GetBreedings($db, $log)
 {
-	$log -> addInfo("Something interesting happened");
-	foreach($dbw->query('SELECT * from wp_users') as $row) 
-	{
-		print_r($row);
-	}
+	$log -> addInfo("Getting breedings.");
+	$stmt = $db->prepare("SELECT grupa, o.imie, o.nazwisko, o.miejscowosc, o.region, o.email, o.tel_kom, o.tel_stac, h.przydomek, h.pisany, h.data_rej, r.rasa, r.breed FROM osoba o LEFT JOIN czlonek_hodowla ch ON o.czlonek = ch.nr_leg LEFT JOIN czlonek cz ON o.czlonek = cz.nr_leg LEFT JOIN logowanie l ON o.czlonek = l.nr_leg LEFT JOIN hodowla h ON ch.nr_hod = h.nr_hod LEFT JOIN rasa_hodowla rh ON h.nr_hod = rh.nr_hod LEFT JOIN rasa r ON rh.id_rasa = r.id_rasa WHERE czlonek IS NOT NULL AND ch.nr_leg IS NOT NULL AND l.status != 'blokada' AND (cz.data_stop IS NULL OR cz.data_stop = '') AND rasa IS NOT NULL AND (h.data_wrej IS NULL OR h.data_wrej = '') ORDER BY grupa, r.rasa, h.przydomek, o.nazwisko, o.imie;");
+	$stmt->execute();
+	$breedings = $stmt->fetchAll();
+	
+	return json_encode($breedings);
+}
+
+function GetStuds($db, $log)
+{
+	$log -> addInfo("Getting studs.");
+	$stmt = $db->prepare("SELECT grupa, p.fullname, p.tytul, p.wyszkolenie, p.data_ur, r.rasa, r.breed, m.masc, m.colour, o.imie, o.nazwisko, o.email, o.tel_kom, o.tel_stac FROM pies p LEFT JOIN rasa r ON p.rasa = r.id_rasa LEFT JOIN masc m ON p.masc = m.id_masc LEFT JOIN wlasciciel_pies wp ON wp.id_pies = p.id_pies LEFT JOIN osoba o ON o.id_osoba = wp.id_osoba LEFT JOIN czlonek cz ON o.czlonek = cz.nr_leg LEFT JOIN logowanie l ON o.czlonek = l.nr_leg WHERE o.czlonek IS NOT NULL AND l.status != 'blokada' AND (cz.data_stop IS NULL OR cz.data_stop = '') AND r.rasa IS NOT NULL AND (p.data_sm IS NULL OR p.data_sm = '') AND plec = 'pies' AND (nadane_upr = 'pelne' OR nadane_upr = 'rok') ORDER BY grupa, r.rasa, p.fullname, o.nazwisko, o.imie;");
+	$stmt->execute();
+	$studs = $stmt->fetchAll();
+	
+	return json_encode($studs);
+}
+
+function GetLitters($db, $log)
+{
+	$log -> addInfo("Getting litters.");
+	$stmt = $db->prepare("SELECT grupa, h.przydomek as hodowla, m.data_ur, r.rasa, r.breed FROM osoba o LEFT JOIN czlonek_hodowla ch ON o.czlonek = ch.nr_leg LEFT JOIN czlonek cz ON o.czlonek = cz.nr_leg LEFT JOIN logowanie l ON o.czlonek = l.nr_leg LEFT JOIN hodowla h ON ch.nr_hod = h.nr_hod LEFT JOIN miot m ON m.przydomek = h.nr_hod LEFT JOIN rasa r ON m.rasa = r.id_rasa WHERE ch.nr_leg IS NOT NULL AND l.status != 'blokada' AND (cz.data_stop IS NULL OR cz.data_stop = '') AND r.rasa IS NOT NULL AND (h.data_wrej IS NULL OR h.data_wrej = '') AND m.data_ur > CURDATE() - INTERVAL 7 MONTH GROUP BY m.nr_miot ORDER BY grupa, r.rasa, m.data_ur desc, h.przydomek;");
+	$stmt->execute();
+	$litters = $stmt->fetchAll();
+	
+	return json_encode($litters);
+}
+
+function GetExhibitionData($db, $log)
+{
+	$log -> addInfo("Getting exhibition data for form.");
+	//get exhibitions
+	$stmt = $db->prepare("SELECT nazwa as id, pelna_nazwa as name from wystawa where data >= CurDate() + interval 3 day AND data <= CurDate() + interval 91 day order by data;");
+	$stmt->execute();
+	$exhibitions = json_encode($stmt->fetchAll());
+	//get breeds
+	$stmt = $db->prepare("SELECT id_rasa as id, rasa as name FROM rasa ORDER BY rasa;");
+	$stmt->execute();
+	$breeds = json_encode($stmt->fetchAll());
+	//get color
+	$stmt = $db->prepare("SELECT id_masc as id, masc as name FROM masc ORDER BY masc;");
+	$stmt->execute();
+	$colors = json_encode($stmt->fetchAll());
+	//get countries
+	$stmt = $db->prepare("SELECT id_panstwo as id, kraj as name FROM panstwo;");
+	$stmt->execute();
+	$countries = json_encode($stmt->fetchAll());
+	
+	$result = '{"exhibition":' . $exhibitions . ',"breed":' . $breeds . ',"color":' . $colors . ',"country":' . $countries . '}';
+
+	return $result;
 }
 
 function GetToken($username, $password, $dbw, $log, $secret)
@@ -48,4 +94,15 @@ function GetToken($username, $password, $dbw, $log, $secret)
 	$log -> addInfo("Token for user: " . $username . " created succesfuly. Generated token: " . $token);
 	
 	return $token;
+}
+
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
 }
--- src/public/index.php
+++ src/public/index.php
@@ -1,39 +1,40 @@
 <?php
-	header('Access-Control-Allow-Origin: https://pfk.org.pl');
-	header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-	header('Access-Control-Allow-Headers: X-Custom-Header');
-
+	header('Content-Type: text/javascript; charset=utf8');
 	date_default_timezone_set('Europe/Warsaw');
 	require '../vendor/autoload.php';
 	$settings = require '../private/local.php';
 	include('functions.php');
 
 	$app = new \Slim\Slim($settings);
+	//set up logging
 	$app->container->singleton('log', function () {
 		$logger = new \Monolog\Logger('PFK_API');
-		$file_handler = new \Monolog\Handler\RotatingFileHandler("../logs/app.log", 5);
+		$file_handler = new \Monolog\Handler\RotatingFileHandler("../logs/app.log", 3);
 		$logger->pushHandler($file_handler);
 		return $logger;
 	});
+	//set up JWT tokens
 	$app->add(new \Slim\Middleware\JwtAuthentication([
 		"path" => "/",
 		"logger" => $app->log,
-		"passthrough" => "/token",	
+		"passthrough" => ["/token", "/breedings", "/studs", "/litters", "/exhibition"],	
 		"secret" => $settings['settings']['secret'],
 		"callback" => function ($options) use ($app) {
 			$app->jwt = $options["decoded"];
         }
 	]));
 
+	//set up PFK DB connection
 	$app->container->singleton('db', function ($c) 
 	{
 		try
 		{
 			$db = $c['settings']['db'];
-			$pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+			$pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'] . ";charset=utf8",
 			$db['user'], $db['pass']);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			//$pdo->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 			return $pdo;
 		}
 		catch(Exception $e){
@@ -41,12 +42,13 @@
 		}
 	});
 	
+	//set up Wordpress DB connection
 	$app->container->singleton('dbw', function ($c) 
 	{
 		try
 		{
 			$db = $c['settings']['dbw'];
-			$pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+			$pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'] . ";charset=utf8",
 			$db['user'], $db['pass']);
 			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
@@ -58,29 +60,136 @@
 		}
 	});
 	
+	//set up secret
 	$app->container->singleton('secret', function ($c) 
 	{
 		return $c['settings']['settings']['secret'];
 	});
 	
-	$app->get('/hello/:name', function ($name) use ($app) 
+	//GET breedings
+	$app->get('/breedings', function () use ($app) 
 	{
+		$log = $app->log;
 		try
 		{
-			$log = $app->log;
+			$req = $app->request;
+			$referer = $req->getReferrer();
+			if(strrpos($referer, "https://pfk.org.pl") === false)
+			{
+				$app->response->status(401);
+				return;
+			}
 			$db = $app->db;
-			$dbw = $app->dbw;
-			if (in_array("administrator", $app->jwt->scope)) 
+			/*if (in_array("administrator", $app->jwt->scope)) 
 			{
 				echo "super";
 			} 
 			else 
 			{
-				/* No scope so respond with 401 Unauthorized */
-			//	$app->response->status(401);
+				No scope so respond with 401 Unauthorized 
+				$app->response->status(401);
 			}
+			*/
 			
-			hello($db, $log, $dbw);	
+			$breedings = GetBreedings($db, $log);
+			echo $breedings;
+		}
+		catch(Exception $e)
+		{
+			$log -> addError($e->getMessage());
+			$app->response->status(500);
+		}
+	});
+	
+	//GET studs
+	$app->get('/studs', function () use ($app) 
+	{
+		$log = $app->log;
+		try
+		{
+			$req = $app->request;
+			$referer = $req->getReferrer();
+			if(strrpos($referer, "https://pfk.org.pl") === false)
+			{
+				$app->response->status(401);
+				return;
+			}
+			$db = $app->db;			
+			$studs = GetStuds($db, $log);
+			echo $studs;
+		}
+		catch(Exception $e)
+		{
+			$log -> addError($e->getMessage());
+			$app->response->status(500);
+		}
+	});
+	
+	//GET litters
+	$app->get('/litters', function () use ($app) 
+	{
+		$log = $app->log;
+		try
+		{
+			$req = $app->request;
+			$referer = $req->getReferrer();
+			if(strrpos($referer, "https://pfk.org.pl") === false)
+			{
+				$app->response->status(401);
+				return;
+			}
+			$db = $app->db;			
+			$litters = GetLitters($db, $log);
+			echo $litters;
+		}
+		catch(Exception $e)
+		{
+			$log -> addError($e->getMessage());
+			$app->response->status(500);
+		}
+	});
+	
+	//GET exhibition data
+	$app->get('/exhibition', function () use ($app) 
+	{
+		$log = $app->log;
+		try
+		{
+			$req = $app->request;
+			$referer = $req->getReferrer();
+			if(strrpos($referer, "https://pfk.org.pl") === false)
+			{
+				$app->response->status(401);
+				return;
+			}
+			$db = $app->db;			
+			$exhibitionData = GetExhibitionData($db, $log);
+			echo $exhibitionData;
+		}
+		catch(Exception $e)
+		{
+			$log -> addError($e->getMessage());
+			$app->response->status(500);
+		}
+	});
+	
+	//POST exhibition data
+	$app->post('/exhibition', function () use ($app) 
+	{
+		$body = $app->request->getBody();
+		$log = $app->log;
+		try
+		{
+			$req = $app->request;
+			$referer = $req->getReferrer();
+			if(strrpos($referer, "https://pfk.org.pl") === false)
+			{
+				//$app->response->status(401);
+				//return;
+			}
+			$db = $app->db;			
+			//$exhibitionData = GetExhibitionData($db, $log);
+			echo $body;
 		}
 		catch(Exception $e)
 		{
